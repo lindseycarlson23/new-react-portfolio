067f8c729f2fe54a6270537452d3022fc472cd2f
lindseycarlson23 <144490541+lindseycarlson23@users.noreply.github.com>
2024-01-16 17:48:40 -0600

    added return to hero section

--- src/Pages/Home/HeroSection.jsx
+++ src/Pages/Home/HeroSection.jsx
@@ -1,1 +1,26 @@
-export default 
+export default function HeroSection() {
+  return (
+    <section id="heroSection" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Lindsey</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section--title--color">Full Stack</span>
+            {""}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section--description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            <br /> Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+            Provident sapiente iusto excepturi.
+          </p>
+        </div>
+        <button className="btn btn-primary">Get in Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/profile_pic.jpg" alt="Hero Section" />
+      </div>
+    </section>
+  );
+}
