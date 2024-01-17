ef9b720e9a3133dc302b973a30ce522a0b26b72d
lindseycarlson23 <144490541+lindseycarlson23@users.noreply.github.com>
2024-01-17 15:37:33 -0600

    added aboutme.jsx content

--- src/Pages/Home/AboutMe.jsx
+++ src/Pages/Home/AboutMe.jsx
@@ -1,1 +1,25 @@
-
+export default function AboutMe() {
+  return (
+    <section id="AboutMe" className="about--section">
+      <div className="about--section--img">
+        <img src="./img/about-me.png" alt="About Me" />
+      </div>
+      <div className="hero--section--content--box about--section--box">
+        <div className="hero--section--content">
+          <p className="section--title">About</p>
+          <h1 className="skills--section--heading">About Me</h1>
+          <p className="hero--section-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+            obcaecati eaque velit maxime quaerat necessitatibus, consequuntur
+            eum atque tempore alias ullam corporis earum placeat corrupti soluta
+            reprehenderit ratione nostrum minima.
+          </p>
+          <p className="hero--section-description">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+            obcaecati eaque velit maxime quaerat necessitatibus.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
